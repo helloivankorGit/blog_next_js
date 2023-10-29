4fa9c99765f30c8989e7068aa15a0addc34a7f50
ivankor <ivan.korovkin@wdg.com.ua>
2023-10-29 09:27:55 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    async rewrites() {
-      return [
-        // Rewrite /blog/{id} to /blog/[id]
-        {
-          source: '/blog/:id',
-          destination: '/blog/[id]',
-        },
-      ];
-    },
-  };
+module.exports = nextConfig
